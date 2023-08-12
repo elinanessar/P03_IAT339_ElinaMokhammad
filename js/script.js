09d936e9123a0db34f306a0f091134946e241451
elinanessar <elinanessar@gmail.com>
2023-08-12 19:28:48 +0600

    js fixed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
-// script.js
-
-const message = document.getElementById('message');
-const overlay = document.getElementById('overlay');
-
-let index = 0;
-
-function showNextLetter() {
-  if (index < message.textContent.length) {
-    message.style.opacity = 1;
-    message.innerHTML += message.textContent.charAt(index);
-    index++;
-    setTimeout(showNextLetter, 100); // Adjust the timing between letters
-  } else {
-    setTimeout(() => {
-      overlay.style.opacity = 0;
-      setTimeout(() => {
-        overlay.style.display = 'none';
-      }, 500); // Adjust the timing before overlay removal
-    }, 1000); // Adjust the timing before overlay fades out
-  }
-}
-
-showNextLetter();
+var typed = new Typed(".auto-type", {
+    strings: ["Hi there!", "Elina here :)", "Hope you enjoy!"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+})
