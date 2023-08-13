55753e3084b7c454221474162350155070b7ee52
elinanessar <elinanessar@gmail.com>
2023-08-13 07:44:04 +0600

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-console.log("Script is running!");
-var typed = new Typed(".auto-type", {
-    strings: ["Hi there!", "Elina here :)", "Hope you enjoy!"],
-    typeSpeed: 150,
-    backSpeed: 100,
-    loop: true
-})
+// console.log("Script is running!");
+// var typed = new Typed(".auto-type", {
+//     strings: ["Hi there!", "Elina here :)", "Hope you enjoy!"],
+//     typeSpeed: 150,
+//     backSpeed: 100,
+//     loop: true
+// })
+
+var messageArray = ["Hi there!"];
+var textPosition = 0;
+var speed = 100;
+
+typewriter = () => {
+  document.querySelector("#message").innerHTML =
+    messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+
+  if (textPosition++ != messageArray[0].length) 
+    setTimeout(typewriter, speed);
+};
+
+window.addEventListener("load", typewriter);
